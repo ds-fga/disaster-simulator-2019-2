6e071c8818d9c105eb310cab287c29bd7fc2958f
Brian Pina <brian.pina2001@gmail.com>
2019-10-16 17:53:00 -0300

    Implementação de tabs na tela de ciência
    Co-authored-by: Alan Marques Sousa <alan.sousa@protonmail.com>
    Co-authored-by: Ciro Costa de Araújo <ciro.fga@gmail.com>
    Co-authored-by: Arthur Alves de Matos <artziz4576@gmail.com>

---
+++ web/src/science/components/TabContent.tsx
@@ -0,0 +1,6 @@
+export class TabContent {
+    view (vnode) {
+        var children = vnode.children;
+        return <div>{children}</div>
+    }
+}
--- web/src/science/science.tsx
+++ web/src/science/science.tsx
@@ -1,12 +1,50 @@
 import m from 'mithril';
 import {Window, Tab, Tabs, Btn, Sidebar} from '../ui';
 
+var tags = [
+    {
+        nome: "Overview",
+        conteudo: "tab1",
+    },
+    {
+        nome: "Nuclear",
+        conteudo: "tab2",
+    },
+    {
+        nome: "Biológico",
+        conteudo: "tab3",
+    },
+    {
+        nome: "Energia",
+        conteudo: "tab4",
+    },
+    {
+        nome: "Transporte",
+        conteudo: "tab5",
+    }
+
+];
 
 /**
  * Componente para janela com árvore tecnológica e de desenvolvimento científico.
  */
 export class Science {
     view () {
-        return <Window>Ciência</Window>
+        return <Window>
+            <Tabs>
+                <Tab title={<button class="nes-btn Science__btn">Overview</button>}>
+                </Tab>
+                <Tab title={<button class="nes-btn is-warning Science__btn">Nuclear</button>}>
+                </Tab>
+                <Tab title={<button class="nes-btn is-success Science__btn">Biologico</button>}>
+                </Tab>
+                <Tab title={<button class="nes-btn is-primary Science__btn">Energético</button>}>
+                </Tab>
+                <Tab title={<button class="nes-btn Science__btn">Transporte</button>}>
+                </Tab>
+
+            </Tabs>
+            
+        </Window>
     }
 }
