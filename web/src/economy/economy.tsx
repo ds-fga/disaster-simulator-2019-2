29062038145136188be7d8768d004a05323ee5cd
jombas <jaummoura15@gmail.com>
2019-10-17 21:11:25 -0300

    Adicionado as mudandças feitas na aula de quarta-feira

--- web/src/economy/economy.tsx
+++ web/src/economy/economy.tsx
@@ -1,12 +1,25 @@
 import m from 'mithril';
 import {Window, Tab, Tabs, Btn, Sidebar} from '../ui';
 
-
 /**
  * Componente para janela de economia.
  */
 export class Economy {
     view () {
-        return <Window>Economia</Window>
+        return <Window>
+            <Sidebar title="Economia"/>
+            <Tabs>
+                <Tab title="Ações">
+                    
+                </Tab>
+                <Tab title="Mercado">
+                    
+                </Tab>
+                <Tab title="Inventário">
+                    
+                </Tab>
+            </Tabs>
+            
+        </Window>
     }
 }
