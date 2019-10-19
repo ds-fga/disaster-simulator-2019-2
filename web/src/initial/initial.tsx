7a27dedf81abe9257225dab57374708696a0c3f3
DouglasMonteles <silvadouglas328@gmail.com>
2019-10-19 16:03:53 -0300

    Menu lateral esquerdo criado

--- web/src/initial/initial.tsx
+++ web/src/initial/initial.tsx
@@ -2,9 +2,53 @@ import m = require('mithril');
 import {Window, Tab, Tabs, Btn, Sidebar} from '../ui';
 
 
-/**
- * Componente para janela inicial.
- */
+var React = {
+    createElement: m
+};
+
+let leftMenuItens = {
+    "nome": ["teste 1", "teste 2"],
+    "caminho": ["teste.html", "teste2.html"],
+    "icone": ["icon icon-link", "icon icon-link"],
+}
+
+
+function elements() {
+    var list_left = [], cont, itens = leftMenuItens;
+
+    for(cont=0; cont<itens.nome.length; cont ++) {
+        list_left.push(m('li', {class: 'menu-item'}, [
+            m('a', {href: itens.caminho[cont]}, [
+                m('i', {class: itens.icone[cont]})
+            ], itens.nome[cont])
+        ])) 
+    }
+    return list_left;
+}
+
+function leftDashboardElements() {
+    return m('ul', {class: 'menu'}, [
+        m('li', {class: 'divider'}),
+        elements()
+    ])
+}
+
+function leftDashboard() {
+    return m('div', {class: 'columns text-center'}, [
+        m('div', {class: 'column col-1'}, [
+            leftDashboardElements()
+        ])
+    ]);
+}
+
+var app = document.querySelector('#app');
+
+
+m.mount(app, {
+    view: () => leftDashboard()
+});
+    
+
 export class Initial {
     view () {
         return <Window>Tela Inicial</Window>;
