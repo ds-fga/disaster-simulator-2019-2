91d4dd1d72957ef83491fc657545efc59823310e
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2019-10-21 18:03:27 -0300

    Adicionou Cultura

--- web/src/culture/culture.tsx
+++ web/src/culture/culture.tsx
@@ -9,16 +9,17 @@ let imagens = {
     naruto: 'https://scontent.fbsb9-1.fna.fbcdn.net/v/t1.0-9/67718150_865004160518655_3293319019209162752_n.png?_nc_cat=107&_nc_oc=AQkdx6JccuXAmaDyw0cRlesFGZ_FCUZlo0rHKYZB29iEd1mQVN6yR_xzn9vKb_dzaWjPGHJ_Kpol1aJu0llTka9F&_nc_ht=scontent.fbsb9-1.fna&oh=b19735787751f3d060e88e2e67b33d78&oe=5E603058',
 
 }
+
 export class Culture {
     view () {
         return (
             <Window>
-                <Sidebar src = {imagens.naruto} title = 'Uzumaki Narutão'/>
+                <Sidebar/>
                     <Tabs>
                         <Tab title = 'EAE'>
                             <ul class = 'Culture-naruto' style = 'width: 50%'>
-                                <li>Oie <Btn>qualquer coisa</Btn></li> 
-                                <li>OI</li>
+                                <li>Oie</li> 
+                                <li id = 'li'>OI</li>
                             </ul>
                         </Tab>
                         <Tab title = 'OI'>Sabanai V.I.A.D.O</Tab>
@@ -28,4 +29,3 @@ export class Culture {
         );
     }
 }
-
