7e0e96123b5a56a12eb7eef2b6814d7a5ab46388
Alan Marques Sousa <alan.sousa@protonmail.com>
2019-10-24 08:08:28 -0300

    Add filter in the tabs

--- web/src/science/science.tsx
+++ web/src/science/science.tsx
@@ -20,51 +20,80 @@ export class Science {
                     title: "Nuclear 1",
                     price: "50",
                     type: "is-warning",
-                    spec: "Permite pesquisa básica em energia nuclear"
+                    spec: "Permite pesquisa básica em energia nuclear",
+                    type_tech: "nuclear"
                 },
                 {
                     title: "Nuclear 2",
                     price: "100",
                     type: "is-warning",
-                    spec: "Permite pesquisa média em energia nuclear"
+                    spec: "Permite pesquisa média em energia nuclear",
+                    type_tech: "nuclear"
                 },
                 {
                     title: "Nuclear 3",
                     price: "150",
                     type: "is-warning",
-                    spec: "Permite pesquisa avançada em energia nuclear"
+                    spec: "Permite pesquisa avançada em energia nuclear",
+                    type_tech: "nuclear"
                 },
                 {
                     title: "Biológico 1",
                     price: "50",
                     type: "is-success",
-                    spec: "Permite pesquisa básica em biotecnologia"
+                    spec: "Permite pesquisa básica em biotecnologia",
+                    type_tech: "biológico"
                 },
                 {
                     title: "Biológico 2",
                     price: "100",
                     type: "is-success",
-                    spec: "Permite pesquisa média em biotecnologia"
+                    spec: "Permite pesquisa média em biotecnologia",
+                    type_tech: "biológico"
                 },
                 {
                     title: "Biológico 3",
                     price: "150",
                     type: "is-success",
-                    spec: "Permite pesquisa avançada em biotecnologia"
+                    spec: "Permite pesquisa avançada em biotecnologia",
+                    type_tech: "biológico"
                 }
             ],
 
+            techsFilter: {},
             currentArea: "",
             currentTech: "",
             searchbox: "",
             currentTechSpec: ""
 
         }
+    } 
+
+    // oncreate() {
+    //   const newTechsFilter = {};
+    //   this.estado.techs.forEach((tech) => {
+    //     if (!(tech.type_tech in newTechsFilter)) {
+    //       newTechsFilter[tech.type_tech] = [];
+    //     }
+    //     newTechsFilter[tech.type_tech].push(tech);
+    //   });
+    //   this.estado.techsFilter = newTechsFilter;
+    // }
+
+    oninit () {
+        const newTechsFilter = {};
+      this.estado.techs.forEach((tech) => {
+        if (!(tech.type_tech in newTechsFilter)) {
+          newTechsFilter[tech.type_tech] = [];
+        }
+        newTechsFilter[tech.type_tech].push(tech);
+      });
+      this.estado.techsFilter = newTechsFilter;
     }
 
     view () {
 
-        const { techs, searchbox, currentTech } = this.estado;
+        const { techs, searchbox, currentTech, techsFilter } = this.estado;
         const filteredtechs = techs.filter(tech => tech.title.toLowerCase().includes(searchbox.toLowerCase()))
 
         return <Window class="science">
@@ -88,15 +117,27 @@ export class Science {
                         </TechList>    
                     </Tab>
                     <Tab class="science__tabs" title={<button class="nes-btn is-warning Science__btn">Nuclear</button>}>
+                      {(techsFilter['nuclear'] || []).map((tech) => (
+                        <Tech title={tech.title} spec={tech.spec} money={tech.price} type={tech.type || ""} />
+                      ))}
                     </Tab>
                     <Tab class="science__tabs" title={<button class="nes-btn is-success Science__btn">Biológico</button>}>
+                      {(techsFilter['biológico'] || []).map((tech) => (
+                        <Tech title={tech.title} spec={tech.spec} money={tech.price} type={tech.type || ""} />
+                      ))}
                     </Tab>
                     <Tab class="science__tabs" title={<button class="nes-btn is-primary Science__btn">Energético</button>}>
+                      {(techsFilter['energético'] || []).map((tech) => (
+                        <Tech title={tech.title} spec={tech.spec} money={tech.price} type={tech.type || ""} />
+                      ))}
                     </Tab>
                     <Tab class="science__tabs" title={<button class="nes-btn is-error Science__btn">Transporte</button>}>
+                    {(techsFilter['transporte'] || []).map((tech) => (
+                        <Tech title={tech.title} spec={tech.spec} money={tech.price} type={tech.type || ""} />
+                      ))}
                     </Tab>
                 </Tabs>
-
+                
                 <TechInfo title={this.estado.currentTech||"Escolha uma tecnologia"}>
                     {this.estado.currentTechSpec}
                 </TechInfo>
