3bfa3e3912096c6cbbd04f06a9743d16f9d48e86
Erick levy <erickfga23@gmail.com>
2019-11-10 18:38:17 -0300

    resolvendo conflito da branch minha com a do rodrigo

--- web/src/initial/initial.tsx
+++ web/src/initial/initial.tsx
@@ -1,7 +1,139 @@
-import m = require('mithril');
-import {Window, Tab, Tabs, Btn, Sidebar} from '../ui';
 
+    createElement: m
+};
 
+<<<<<<< master
+/** A parte a seguir do código server para documentar o nome de cada botão,
+ * O redirecionamento feito após clica-lo
+ * E o icone respectivo
+ */
+
+ let leftMenuItens = {
+    "nome": ["Temperatura", "Política", "Conspiração","Economia"],
+    "caminho": ["teste.html", "teste2.html", "teste2.html", "teste.html"],
+    "icone": [frask, politic, conspirancy, money],
+}
+
+let barMenuItens = {
+    "nome": ["teste 1", "teste 2", "teste 3"],
+    "caminho": ["teste.html", "teste2.html", "teste3.html"],
+    "icone": ["icon icon-link", "icon icon-link", "icon icon-link"],
+}
+let rightMenuItens = {
+    "nome": ["Gráfico", "Terra", "Conspiração", "População", "Configuraçoẽs"],
+    "caminho": ["teste.html", "teste2.html", "teste2.html", "teste.html", "teste2.html", "teste2.html"],
+    "icone": [graphic, earth, reptle, people, settings],
+}
+
+/**A parte a seguir é para auxiliar
+ * O listamento de cada botão
+ * Para cada Dashboard
+ */
+
+function elementsColumn(itens, classli) {
+    var list = [];  
+    for(let i = 0; i < itens.nome.length; i++) {
+        list.push(m('li', {class: classli}, [
+            m('a', {href: itens.caminho[i]}, [
+                m('img', {class: 'mr-5', src: itens.icone[i]})
+            ], itens.nome[i])
+        ]))
+    }
+    return list;
+}
+
+/** A parte a seguir é sobre o cabeçalho do jogo
+ * Será apresentado o título do jogo
+*/
+
+function header(){
+    return m('div', {class: 'header'}, [
+        m('div', {class: 'nes-container is-rounded is-dark with-title'}, [
+            m('p', {class: 'title'}, 'Disaster Simulation'),
+            m('p', {class: 'text-gray'}, 'Projeto da disciplina de Desenvolvimento de Software')
+        ])
+    ])
+}
+
+/**A parte a seguir é sobre o status
+ * Deve apresentado aqui o status do jogo
+ * Ano, Variação de Temperatura, Dinheiro e Quantidade de pessoas
+*/
+
+function status(){
+    return m('div', {class: 'status nes-container is-rounded is-dark with-title'},[
+        m('p', {class: 'title'}, 'Aqui fica o Status!')
+    ])
+}
+
+/**A parte a seguir é sobre a coluna da esquerda
+ * E chama seus respectivos itens
+*/
+
+function leftDashboard() {
+    return m('div', {class: 'leftDash'}, [
+        m('ul', {class: 'menu align-left nes-container is-rounded is-dark with title'}, [
+            'Ações',
+            m('li', {class: 'divider'}),
+            elementsColumn(leftMenuItens, 'menu nes-container is-dark')
+        ])
+    ])
+}
+
+/** A parte a seguir é a main do projeto
+ * A parte onde ficará globo
+ */
+
+function main() {
+    return m('div', {class: 'main'}, [
+        'Corpo da Main',
+        m('div', {style: {'text-align': 'center',
+                            'text-content': 'center'}}, [
+            m('img', {src: big_earth})
+        ]),
+    ])
+}
+
+/** A parte a seguir é sobre a coluna da direita
+ * E chama seus respectivos itens
+*/
+
+function rightDashboard() {
+    return m('div', {class: 'rightDash'}, [
+        m('ul', {class: 'menu align-right nes-container is-rounded is-dark with title'}, [
+            'Informações',
+            m('li', {class: 'divider'}),
+            elementsColumn(rightMenuItens, 'menu nes-container is-dark')
+        ])
+    ])
+}
+
+/**A parte a seguir é sobre o rodapé do jogo
+ * Nele, terá 3 barras, em uma delas conterá
+ * O status pessoais do jogador
+ * Outra apresentará Notícias
+ * E na ultima apresentará os créditos do jogo
+*/
+
+function footer(){
+    return m('div', {class: 'footer nes-container is-rounded is-dark with title'}, [
+        elementsColumn(barMenuItens, 'menu inline nes-container is-dark')
+    ])
+}
+
+/** A parte a seguir inicia e ordena a tela */
+
+function join() {
+    var join = [
+        header(),
+        status(),
+        leftDashboard(),
+        main(),
+        rightDashboard(),
+        footer(),
+    ]
+    return <section class="Initial nes-container is-dark">{join}</section>;
+=======
 var React = {
     createElement: m
 };
@@ -104,6 +236,7 @@ function join() {
     ]
 
     return <div class="columns">{join}</div>;
+
 }
 
 var app = document.querySelector('#menu-lateral');
@@ -118,4 +251,7 @@ export class Initial {
     view () {
         return <Window>{leftDashboard()}</Window>;
     }
-}*
+<<<<<<< master
+}*/
+
+
