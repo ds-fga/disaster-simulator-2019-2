b11a7d5b442da024c77f2c76e108ff37cdeb12fa
jombas <jaummoura15@gmail.com>
2019-11-11 20:26:33 -0300

    Botões funcionando :D

--- web/src/economy/economy.tsx
+++ web/src/economy/economy.tsx
@@ -1,25 +1,81 @@
 import m from 'mithril';
-import {Window, Tab, Tabs, Btn, Sidebar} from '../ui';
+import {Window, Tab, Tabs, Btn, Sidebar, VScroll} from '../ui';
+//import exampleImg from './teste.jpg";
+//colocar imagem. exampleImg nome <img src={exampleImg}></img>
 
 /**
  * Componente para janela de economia.
  */
 export class Economy {
     view () {
-        return <Window>
-            <Sidebar title="Economia"/>
-            <Tabs>
-                <Tab title="Ações">
-                    
-                </Tab>
-                <Tab title="Mercado">
-                    
-                </Tab>
-                <Tab title="Inventário">
-                    
-                </Tab>
-            </Tabs>
-            
-        </Window>
+        return <div>
+            <Window>
+                <Sidebar title="Economia" points="4"/>
+                <Tabs>
+                    <Tab title="Ações">
+
+                        <div class="flex-container">
+                            <div>
+                                <ExpandirCard title="teste">
+                                    <h1>aaaaaaa</h1>
+                                </ExpandirCard>
+                            </div>
+                            <div>
+                                <ExpandirCard title="teste2">
+                                    <h1>aaaaaaa</h1>
+                                </ExpandirCard>
+                            </div>
+                        </div>
+
+                    </Tab>
+                    <Tab title="Mercado">
+                        
+                    </Tab>
+                    <Tab title="Inventário">
+                        
+                    </Tab>
+                </Tabs>
+                
+            </Window>
+        </div>
     }
 }
+
+export class ExpandirCard {
+    constructor(vnode) {
+        this.expand = false;
+    }
+
+    view (vnode) {
+        var buttonOn = "nes-btn scroll-btn Economy-button";
+        var content;
+        
+        if(this.expand) {
+            content = vnode.children;
+            buttonOn += " Economy-button-on"
+        }
+        else {
+            content = "";
+        }
+
+        return <VScroll class="EconomyCard">
+            <div class="EconomyCard-title">
+                {vnode.attrs.title}
+                <button type="button" class={buttonOn} onclick={() => {this.expand = !this.expand}}>
+                    <span>&lt;</span>
+                </button>
+                <hr></hr>
+            </div>
+        
+            <div class="EconomyCard-content">
+                {content}
+            </div>  
+        </VScroll>
+    }
+}
+
+//criar um div na window pq só le um filho
+//vscroll rolamento
+//vnode.attrs pegar algum atributo passado
+//hr é a linha
+//constructor uma variável é criada localmente
