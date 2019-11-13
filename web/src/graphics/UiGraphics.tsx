57099db4a5ecc2943cda9cdafaac72cff356d221
thiagohdaqw <54081877+thiagohdaqw@users.noreply.github.com>
2019-11-13 19:56:04 -0300

    Classe para os graficos

--- web/src/graphics/UiGraphics.tsx
+++ web/src/graphics/UiGraphics.tsx
@@ -1,5 +1,5 @@
 import m from 'mithril';
-import { Window, Tab, Tabs, Btn, Sidebar, Chart, VScroll, Component } from '../ui';
+import { Window, Tabs, Btn, Sidebar, Chart, VScroll} from '../ui';
 import { MithrilTsxComponent as Component } from 'mithril-tsx-component';
 
 
@@ -33,10 +33,10 @@ export class GraphicTabs extends Elem {
         let background = vnode.attrs.background || "";
         if (background != "")
             graphicswindow.style.background = `url(${vnode.attrs.background})`;
-            graphicswindow.style.backgroundSize = "100%";
+            graphicswindow.style.backgroundSize = "cover";
             graphicswindow.style.backgroundRepeat = "no-repeat";
             graphicswindow.style.backgroundAttachment = "fixed";
-            graphicswindow.style.backgroundPosition = "center top"
+            graphicswindow.style.backgroundPosition = "center center"
             graphicswindow.style.transition = "0.5s";
     }
 
@@ -48,10 +48,10 @@ export class GraphicTabs extends Elem {
                 cls += i == selected ? " is-selected" : "";
                 let btn = e.attrs.btn || "warning";
                 btn = i == selected ? "primary" : btn;
-                return <div class={cls}
-                    onclick={() => {self.selected = i; }}>
-                    <GraphicsButton btn={btn} onclick={e.attrs.onclick || ""}>{e.attrs.title}</GraphicsButton>
-                </div>
+                return <div class={cls}>
+                    <button type="button" class={`Graphics nes-btn is-${btn}`} onclick={() => {self.selected = i; }}> {e.attrs.title}</button>
+                    </div>
+
             });
     return <div class="Graphics Tab-head">{titles}</div>;
     }
@@ -67,12 +67,13 @@ export class GraphicTabs extends Elem {
 
 
 interface Sattrs extends IGenericAttrs {
-    btn?: "primary" | "success" | "warning" | "error" | "disabled" | "normal";
-    title: string;
     background?: string;
+    btn?: "primary" | "success" | "warning" | "error" | "disabled" | "normal";
+    title?: string;
+    
 }
 
-export class Tab extends Component<Sattrs> {
+export class GraphicTab extends Component<Sattrs> {
 
 
     view(vnode) {
@@ -80,10 +81,7 @@ export class Tab extends Component<Sattrs> {
             {vnode.children}
         </div>
     }
-};
-
-// tipos de butoes
-
+}
 
 export class GraphicsButton extends Component<Sattrs>{
    view(vnode: m.Vnode<Sattrs>) {
@@ -91,3 +89,125 @@ export class GraphicsButton extends Component<Sattrs>{
         let fonclick = vnode.attrs.onclick || ""
         return <button type="button" class={`Graphics nes-btn is-${extraclass}`} onclick={fonclick}>{vnode.children}</button>
     }
+}
+interface Gattrs{
+    dados?: object;
+}
+export class Gerargraficos extends Component<Gattrs>{
+     
+	selected: number;
+    constructor() {
+        super()
+        this.selected = -1
+    }
+
+    view(vnode: m.Vnode<Gattrs>){
+    {
+            let data = [];
+            let buttons
+            let graph
+            let  graphdata = vnode.attrs.dados
+            for (var ii in graphdata.data) {
+                data.push(ii)
+            }
+
+            buttons = data.map((e, i) => {
+                return <button type="button" class="Graphics nes-btn is-warning" onclick={() => { this.selected = i }}>{e}</button>
+            })
+
+            if (this.selected == -1) {
+                let data1 = graphicData(graphdata, undefined);
+                // para criar um grafico usamos a classe "chart" abaixo
+                graph = <Chart type="line" data={data1} options={data1.options} height={75} />
+            } else {
+                graph = data.map((e, i) => {
+                    if (this.selected == i) {
+                        let data1 = graphicData(graphdata, e);
+                        return <Chart type="line" data={data1} options={data1.options} height={75} />
+                    }
+                })
+            }
+
+            return <div> {graph}
+                <div class="Graphics buttons">
+                    <button type="button" class="Graphics nes-btn is-warning" onclick={() => { this.selected = -1 }}>Global</button>
+                    {buttons}
+                </div>
+                <div class="Graphics audiobutton"><audio autoplay="true" src="http://soundbible.com/grab.php?id=1280&type=mp3" preload="auto" controls></audio></div>
+            </div>
+
+        }
+    }
+}
+// essa a funçao que gera ordena os dados do grafico para depois inserir da classe "chart"
+function graphicData(generalData, specificData) {
+    let graphicdata = {
+        // eixo x
+        labels: generalData.labels,
+        //dados
+        datasets: [],
+        //configuracoes do grafico
+        options: {
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 10,
+                    bottom: 0
+                }
+            },
+            // legendas
+            legend: {
+                labels: {
+                    fontSize: 20,
+                    fontColor: 'white',
+                }
+            },
+            scales: {
+                // eixo y
+                yAxes: [{
+                    ticks: {
+                        fontSize: 25,
+                        fontColor: 'white',
+                    }
+                }],
+                // eixo x
+                xAxes: [{
+                    ticks: {
+                        fontSize: 25,
+                        fontColor: 'white',
+
+                    }
+                }]
+            },
+
+        }
+
+    }
+
+    if (specificData != undefined) {
+        for (var i in generalData.data) {
+            if (i == specificData) {
+                graphicdata.datasets.push({
+                    label: i,
+                    data: generalData.data[i],
+                    backgroundColor: generalData.backgroundColor,
+                    borderColor: generalData.borderColor,
+                    borderWidth: 5
+                })
+            }
+
+        }
+    } else {
+        for (var i in generalData.data) {
+            graphicdata.datasets.push({
+                label: i,
+                data: generalData.data[i],
+                backgroundColor: generalData.backgroundColor,
+                borderColor: generalData.borderColor,
+                borderWidth: 5
+            })
+        }
+    }
+    return graphicdata;
+}
