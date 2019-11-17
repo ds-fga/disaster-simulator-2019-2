c4e654511a05b3778718cda780de0daee1728a1a
jombas <jaummoura15@gmail.com>
2019-11-17 11:51:50 -0300

    Atualizado trabalho

--- web/src/economy/economy.tsx
+++ web/src/economy/economy.tsx
@@ -1,34 +1,101 @@
 import m from 'mithril';
-import {Window, Tab, Tabs, Btn, Sidebar, VScroll} from '../ui';
-import exampleImg from '../../src/economy/teste.jpg';
+import { Window, Tab, Tabs, Btn, Sidebar, VScroll } from '../ui';
+import exampleImg from './teste.jpg';
 //colocar imagem. exampleImg nome <img src={exampleImg}></img>
 
+let data = {
+    lucro: [
+        {
+            title: "Test sdse ",
+            //img: exampleImg,
+            description: "dsjfoisdjfo si jfosij afsohfsauhifasuhosafhuohuoafshoasfhoifosi jfoisdj foisj dfois jdofisodjf osi jfos",
+            preco: "1",
+            attrs: [
+                { name: "attr1", color: 'color: red', points: 1 },
+                { name: "attr2", color: 'color: green', points: -1 },
+            ]
+        },
+        {
+            title: "Test sdse ",
+            //img: exampleImg,
+            description: "dsjfoisdjfo si jfosij fosi jfoisdj foisj dfois jdofisodjf osi jfos",
+            attrs: [
+                { name: "attr1", color: 'color: red', points: 1 },
+                { name: "attr2", color: 'color: green', points: -1 },
+            ]
+        }
+    ],
+    prejuizo: [
+        {
+            title: "Test sdse ",
+            //img: exampleImg,
+            description: "dsjfoisdjfo si jfosij fosi  foisj dfois jdofisodjf osi jfos",
+            attrs: [
+                { name: "attr1", color: 'color: red', points: 1 },
+                { name: "attr2", color: 'color: green', points: -1 },
+            ]
+        }
+    ],
+};
+
+
+function createCard({ title, description, attrs, preco }) {
+    function viewAttr({ name, color, points }) {
+        return <tr style={color}><td>{name}</td> <td>{points}</td></tr>
+    }
+
+    var teste = true;
+    var claseSei = "Economy-botao-direita";
+
+    if (!teste) {
+        claseSei += "Economy-botao-escondido";
+    }
+    //var pontos = 2;
+
+    return <ExpandirCard title={title}>
+        <p>{description}</p>
+        <table>
+            <thead>
+                <tr><h3><th>Nome</th> <th>Pontos</th></h3></tr>
+            </thead>
+            <tbody>
+                {attrs.map(viewAttr)}
+                <tr><h3><th>Preço</th></h3></tr>
+                <tr><td>{preco}</td></tr>
+            </tbody>
+        </table>
+        <div class={claseSei}>
+            <button class="nes-btn" onclick={() => { teste = !teste }}>
+                <span>Comprar</span>
+            </button>
+        </div>
+    </ExpandirCard>
+}
+
 export class Economy {
-    view () {
+    view() {
         return <div>
             <Window>
-                <Sidebar title="Economia" points="4" src={exampleImg}/>
+                <Sidebar title="Economia" points="4" src={exampleImg} />
                 <Tabs>
                     <Tab title="Ações">
                         <div class="flex-container">
                             <div style="flex-grow: 1">
                                 <h1>Lucro</h1>
-                                <ExpandirCard title="Título ">
-                                    <DescricaoDoItem color="red" src={exampleImg}/>
-                                </ExpandirCard>
+                                {data.lucro.map(createCard)}
                             </div>
 
                             <div style="flex-grow: 1">
                                 <h1>Prejuizo</h1>
-                                <ExpandirCard title="teste ">
-                                    <DescricaoDoItem color="red" desc="teste"/>
-                                </ExpandirCard>
+                                {data.prejuizo.map(createCard)}
                             </div>
                         </div>
-
                     </Tab>
 
                     <Tab title="Mercado">
+                        <div>
+                            <testeBotao />
+                        </div>
                     </Tab>
 
                     <Tab title="Inventário">
@@ -42,45 +109,33 @@ export class Economy {
     }
 }
 
-export class DescricaoDoItem {
-    view (vnode) {
-        var color; //aplicar no texto com class={color}
+export class testeBotao {
+    constructor(vnode) {
+        this.pontos = 0;
+    }
 
-        //dependendo do attribute (0 ou 1) ele fica verde ou vermelho
-        vnode.attrs.color === "red" ? color = "Economy-bad-color" : color = "Economy-good-color";
-
+    view() {
         return <div>
-            <div class="flex-container">
-
-                <div style="justify-content: flex-start">
-                    <img src={vnode.attrs.src}></img>
-                </div>
-
-                <div style="flex-grow: 1">
-                    <h3>Descrição:</h3>
-                    <p>{vnode.attrs.desc}</p>
-                    
-                </div>
-                <div style="justify-content: flex-end">
-                    <h3>Atributos:</h3>
-                    <p class={color}>Coisa boa</p>
-                    <p class={color}>Coisa ruim</p>
-                </div>
-            </div>
+            <p>Teste {pontos}</p>
+            <button type="button" onclick={() => { this.pontos += 1 }}>
+                <span>teste</span>
+            </button>
         </div>
     }
 }
 
 export class ExpandirCard {
+    expand: boolean;
+
     constructor(vnode) {
         this.expand = false;
     }
 
-    view (vnode) {
+    view(vnode) {
         var buttonOn = "nes-btn scroll-btn Economy-button";
         var content;
-        
-        if(this.expand) {
+
+        if (this.expand) {
             content = vnode.children;
             buttonOn += " Economy-button-on"
         }
@@ -89,18 +144,18 @@ export class ExpandirCard {
         }
 
         return <VScroll class="EconomyCard">
-            <div class="nes-container with-title is-rounded">   
+            <div class="nes-container with-title is-rounded">
                 <div class="EconomyCard-title">
                     {vnode.attrs.title}
-                    <button type="button" class={buttonOn} onclick={() => {this.expand = !this.expand}}>
+                    <button type="button" class={buttonOn} onclick={() => { this.expand = !this.expand }}>
                         <span>&lt;</span>
                     </button>
                     <hr></hr>
                 </div>
-            
+
                 <div class="EconomyCard-content">
                     {content}
-                </div> 
+                </div>
             </div>
         </VScroll>
     }
@@ -111,3 +166,8 @@ export class ExpandirCard {
 //vnode.attrs pegar algum atributo passado
 //hr é a linha -----------
 //constructor uma variável é criada localmente
+
+
+// <div class="Economy-botao-direita">
+//             <button class="nes-btn">Comprar</button>
+//         </div>
