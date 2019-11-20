ed5620576e98912e8eca270e462837b1981d19b0
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-11-20 16:27:10 -0300

    Adicionou simulação Python

---
+++ app/webapp.py
@@ -0,0 +1,56 @@
+from flask import Flask, escape, request, jsonify
+from . import simulation
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return '''
+<html>
+<body>
+    <script src="https://unpkg.com/mithril/mithril.js"></script>
+    
+    <div id="app"></div>
+    
+    <script>
+    var root = document.getElementById('app')
+    </script>
+</body>
+</html>
+'''
+
+
+@app.route('/api/simulation/step/')
+def step():
+    data = simulation.step()
+    return jsonify({'status': 'OK', 'data': data, 'year': data[0]}) 
+
+
+@app.route('/api/simulation/step/<size>/')
+def step_by(size):
+    for _ in range(int(size) - 1):
+        data = simulation.step()
+    return step() 
+
+
+
+@app.route('/api/simulation/state/')
+def params():
+    return jsonify({
+        'year': simulation.get_var('time'),
+        'population': simulation.get_var('population'),
+        'capital': simulation.get_var('capital'),
+        'temperature_atm': simulation.get_var('t_atm'),
+        'temperature_ocean': simulation.get_var('t_ocean'),
+    }) 
+
+
+@app.route('/api/vars/<name>/')
+def get_var(name):
+    value = simulation.get_var(name.upper())
+    return jsonify({'name': name.lower(), 'value': value})
+
+
+@app.route('/api/series/<name>/')
+def get_series(name):
+    data = simulation.get_series(name)
+    return jsonify({'name': name, 'data': data})
