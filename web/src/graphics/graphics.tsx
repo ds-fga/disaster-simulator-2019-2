82d6701eff8ed298fc9fd41086a608cb2d60eb2b
Thiago <thiagohdaqw@gmail.com>
2019-11-20 21:37:00 -0300

    att

--- web/src/graphics/graphics.tsx
+++ web/src/graphics/graphics.tsx
@@ -1,7 +1,6 @@
 import m from 'mithril';
 import { Window, Tab, Tabs, Btn, Sidebar, Chart, VScroll, Component } from '../ui';
 import './_graphics.scss';
-<<<<<<< HEAD
 import { GraphicsButton, GraphicTabs, GraphicTab, Gerargraficos, BackButton } from './UiGraphics';
 import telaback from './src/telaback.jpg';
 import climaback from './src/climaback.jpg';
@@ -51,23 +50,6 @@ export class Graphics {
                     </GraphicTabs>
                     <GraphicsAudio></GraphicsAudio>
                 </div>
-=======
-
-/**
- * Componente para janela com informações e gráficos sobre o mundo.
- */
-export class Graphics {
-    /**Tabs e como se fosse uma lista de tabs, dai tem que colocar cada Tab dentro de Tabs */
-    view() {
-        return <Window>
-            <VScroll id="Graphics-VScroll">
-                <Tabs>
-                    <Tab title="Clima"><Clima></Clima></Tab>
-                    <Tab title="Economia"><Economia></Economia></Tab>
-                    <Tab title="Politica"><Politica></Politica></Tab>
-                    <Tab title="Energia"><Energia></Energia></Tab>
-                </Tabs>
->>>>>>> att graficos
             </VScroll>
         </Window>
     }
@@ -75,7 +57,6 @@ export class Graphics {
 // algumas dessas classes acima nao estao nesse documento, elas podem ser encontradas no UiGraphics.tsx
 
 
-<<<<<<< HEAD
 // essa eh a variavel que guarda os dados dos graficos
 let pydata = {
     clima: {
@@ -151,22 +132,6 @@ let pydata = {
             "Não Renováveis": "rgba(255,100,10,1)",
         }
     },
-=======
-
-// dados do grafico
-let pydata = {
-    clima: [20, 5, 30, 2, 20, 10],
-    economia: [20, 5, 30, 2, 20],
-    politica: [20, 5, 30, 2, 20],
-    energia: [20, 5, 30, 2, 20],
-};
-// colunas
-let labels = {
-    clima: ['coluna1', 'coluna2', 'coluna3', 'coluna4'],
-    economia: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    politica: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    energia: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
->>>>>>> att graficos
 };
 // essa classe eh o conteudo da tela principal
 class Tela {
@@ -217,7 +182,6 @@ class População {
                 </div>
             </div>
 
-<<<<<<< HEAD
         }
     }
 
@@ -266,147 +230,3 @@ let musica
     }
 }
 
-=======
-
-interface IGenericAttrs {
-    class?: string | string[];
-    id?: string;
-    onclick?: Function;
-}
-// tipos de butoes
-interface Gbtn extends IGenericAttrs {
-    btn?: "primary" | "success" | "warning" | "error" | "disabled" | "normal";
-}
-
-class GraphicsButton extends Component<Gbtn>{
-    view(vnode: m.Vnode<Gbtn>) {
-        let extraclass = vnode.attrs.btn;
-        return <button type="button" class={`nes-btn is-${extraclass}`}>{vnode.children}</button>
-    }
-
-}
-
-class Clima {
-    view() {
-        {
-            let data1 = graphicData(pydata.clima, labels.clima);
-            return <div>
-                <Chart type="bar" data={data1} options={data1.options} height="75" />
-                <div id="Graphics-buttons">
-                    <GraphicsButton btn="primary">Botao1</GraphicsButton>
-                    <GraphicsButton btn="normal">Botao1</GraphicsButton>
-                    <GraphicsButton btn="warning">Botao1</GraphicsButton>
-
-                </div></div>
-
-        }
-    }
-
-class Economia {
-    view() {
-        {
-            let data1 = graphicData(pydata.economia, labels.economia);
-            return <div><Chart type="line" data={data1} options={data1.options} height="75" />
-                <div id="Graphics-buttons">
-                    <GraphicsButton btn="primary">Botao1</GraphicsButton>
-                    <GraphicsButton btn="normal">Botao1</GraphicsButton>
-                    <GraphicsButton btn="warning">Botao1</GraphicsButton>
-                </div></div>
-        }
-    }
-
-class Politica {
-    view() {
-        {
-            let data1 = graphicData(pydata.politica, labels.politica);
-            return <div><Chart type="radar" data={data1} options={data1.options} height="75" />
-                <div id="Graphics-buttons">
-                    <GraphicsButton btn="primary">Botao1</GraphicsButton>
-                    <GraphicsButton btn="normal">Botao1</GraphicsButton>
-                    <GraphicsButton btn="warning">Botao1</GraphicsButton>
-                </div></div>
-        }
-    }
-
-class Energia {
-
-    view() {
-        {
-            let data1 = graphicData(pydata.energia, labels.energia);
-            return <div><Chart type="bar" data={data1} options={data1.options} height="75" />
-                <div id="Graphics-buttons">
-                    <GraphicsButton btn="primary">Botao1</GraphicsButton>
-                    <GraphicsButton btn="normal">Botao1</GraphicsButton>
-                    <GraphicsButton btn="warning">Botao1</GraphicsButton>
-                </div></div>
-        }
-    }
-
-// configuraçoes dos graficos
-function graphicData(data, labels) {
-    let graphicdata = {
-        labels: labels,
-        datasets: [{
-            label: '# of Votes',
-            data: data,
-            backgroundColor: [
-                'hsl(0, 100%, 70%)',
-                'rgba(54, 162, 235, 0.2)',
-                'hsl(75, 100%, 70%)',
-                'rgba(75, 192, 192, 0.2)',
-                'hsl(155, 100%, 70%)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(75, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 10
-        }],
-        
-        //configuracoes do grafico
-        options: {
-
-            layout: {
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 10,
-                    bottom: 0
-                }
-            },
-            // legendas
-            legend: {
-                labels: {
-                    fontSize: 20,
-                    fontColor: 'white',
-                }
-            },
-            scales: {
-            // eixo y
-            
-                yAxes: [{
-                    ticks: {
-                        fontSize: 25,
-                        fontColor:'white',
-                    }
-                }], // eixo x
-                xAxes: [{
-                    ticks: {
-                        fontSize:25,
-                        fontColor: 'white',
-                        
-                    }
-                }]
-            },
-
-        }
-
-    }
-    return graphicdata;
-}
->>>>>>> att graficos
