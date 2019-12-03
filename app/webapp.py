5f4382c531e4902a4bb4a9c9bdfdf9a8f4ab2153
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-12-03 10:51:54 -0300

    Atualizou a API flask

--- app/webapp.py
+++ app/webapp.py
@@ -1,73 +1,77 @@
-from flask import Flask, escape, request, jsonify
-from . import science
+import os
+import json
+from flask import Flask, jsonify
+from flask_cors import CORS
 from . import simulation
+from . import events
+
 app = Flask(__name__)
-
-@app.route('/')
-def root():
-    return '''
-<html>
-<body>
-    <script src="https://unpkg.com/mithril/mithril.js"></script>
-    
-    <div id="app"></div>
-    
-    <script>
-    var root = document.getElementById('app')
-    </script>
-</body>
-</html>
-'''
+CORS(app)
 
 
-@app.route('/api/simulation/step/')
-def step():
-    data = simulation.step()
-    return jsonify({'status': 'OK', 'data': data, 'year': data[0]}) 
+@app.route('/value/state/')
+def get_state():
+    return jsonify(simulation.get_vars())
 
 
-@app.route('/api/simulation/step/<size>/')
-def step_by(size):
-    for _ in range(int(size) - 1):
-        data = simulation.step()
-    return step() 
-
-
-
-@app.route('/api/simulation/state/')
-def params():
-    return jsonify({
-        'year': simulation.get_var('time'),
-        'population': simulation.get_var('population'),
-        'capital': simulation.get_var('capital'),
-        'temperature_atm': simulation.get_var('t_atm'),
-        'temperature_ocean': simulation.get_var('t_ocean'),
-    }) 
-
-
-@app.route('/api/vars/<name>/')
+@app.route('/value/<name>/')
 def get_var(name):
     value = simulation.get_var(name.upper())
     return jsonify({'name': name.lower(), 'value': value})
 
 
-@app.route('/api/series/<name>/')
+@app.route('/series/<name>/')
 def get_series(name):
     data = simulation.get_series(name)
-    return jsonify({'name': name, 'data': data})
+    time = simulation.get_series('time')
+    return jsonify({'name': name, 'time': time, 'data': data})
 
 
-@app.route('/api/series/<name>/')
-def get_series(name):
-    data = simulation.get_series(name)
-    return jsonify({'name': name, 'data': data})
+@app.route('/event/random/')
+def get_event():
+    return jsonify(events.get_event())
 
 
-@app.route('/science/list-techs/')
-def list_techs():
-    return jsonify(sciece.list_techs())
+@app.route('/game/step/')
+def step():
+    data = simulation.step()
+    return jsonify({'status': 'OK', 'data': data, 'year': data[0]})
 
 
-@app.route('/science/buy/<id>/')
-def buy_tech(id):
-    return jsonify(sciece.buy_techs())
+@app.route('/game/step/<size>/')
+def step_by(size):
+    for _ in range(int(size) - 1):
+        data = simulation.step()
+    return step()
+
+
+@app.route('/game/state/')
+def params():
+    return jsonify(simulation.get_game_state())
+
+
+@app.route('/game/save/<name>')
+def save_game(name):
+    path = os.path.abspath(name)
+    state = simulation.get_game_state()
+    with open(path, 'w') as fd:
+        json.dump(state, fd)
+    return jsonify({'status': 'success', 'file': path, 'state': state})
+
+
+@app.route('/game/load/<name>')
+def load_game(name):
+    path = os.path.abspath(name)
+    with open(path, 'r') as fd:
+        state = json.load(fd)
+    simulation.set_game_state(state)
+    return jsonify({'status': 'success', 'file': path, 'state': state})
+
+
+@app.route('/')
+def root():
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, 'index.html')
+    with open(path, 'r') as fd:
+        data = fd.read()
+    return data
