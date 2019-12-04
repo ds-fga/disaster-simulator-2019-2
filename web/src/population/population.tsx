e9a1b49c8f4d0bc3ef58fece0fef0c70c2aa1bab
GuibaGold <guibdourado@gmail.com>
2019-12-04 17:17:55 -0400

    graficos de riqueza e satisfacao

--- web/src/population/population.tsx
+++ web/src/population/population.tsx
@@ -1,5 +1,5 @@
 import m from 'mithril';
-import { Window, Tab, Tabs, Btn, Sidebar } from '../ui';
+import { Window, Tab, Tabs, Btn, Sidebar, Chart } from '../ui';
 import populationIcon from './icon.jpg'
 
 /**
@@ -10,55 +10,81 @@ export class Population {
         return <Window>
             <Sidebar src={populationIcon} title="Populacao" />
             <Tabs>
-                <Tab title={<span><i class="nes-icon coin"></i> riqueza</span>}>
-                    <Riqueza name="foo bar"></Riqueza>
+                <Tab title="Satisfação">
+                    <div id="satisfacaoChart">Satisfação:
+                        <Chart type="pie" data={dataSatisfacao} options={options}></Chart>
+                    </div>
                 </Tab>
-                <Tab title="saude">
-                    <Saude></Saude>
-                </Tab>
-                <Tab title="lazer">
-                    <Lazer></Lazer>
-                </Tab>
-                <Tab title="seguranca">
-                    <Seguranca></Seguranca>
-                </Tab>
-                <Tab title="cultura">
-                    <Cultura></Cultura>
+
+                <Tab title="Riqueza">
+                    <div id="riquezaChart">Riqueza:
+                        <Chart type="pie" data={dataRiqueza} options={options}></Chart>
+                    </div>
+                    
                 </Tab>
             </Tabs>
         </Window>
     }
+
+    
 }
 
 
-class Riqueza {
-    view (vnode) {
-        let name = vnode.attrs.name || "default";
-        return <div>Riqueza {name}</div>
+
+let dataRiqueza = {
+    labels: ['Super Ricos', 'Ricos', 'Classe média', 'Classe média-baixa', 'Pobres', 'Miseravéis'],
+    datasets: [{
+        label: '# of Votes',
+        data: [1/100, 3/100, 20/100, 30/100, 40/100, 6/100],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+let dataSatisfacao = {
+    labels: ['Contentes', 'Infelizes', 'Revoltados', 'Revolucionarios'],
+    datasets: [{
+        label: '# of Votes',
+        data: [50/100, 35/100, 5/100, 10/100,],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+            
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+            
+        ],
+        borderWidth: 1
+    }]
+};
+
+let options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
     }
-}
-
-
-class Saude {
-    view () {
-        return <div>Saude</div>
-    }
-}
-
-class Lazer {
-    view () {
-        return <div>Lazer</div>
-    }
-}
-
-class Seguranca {
-    view () {
-        return <div>Seguranca</div>
-    }
-}
-
-class Cultura {
-    view () {
-        return <div>Cultura</div>
-    }
-}
+};
