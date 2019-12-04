8dda0580141017cc23b9784976a10d80fb7e670a
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2019-12-04 17:12:43 -0300

    Modifiquei culture.py

---
+++ app/culture.py
@@ -0,0 +1,50 @@
+
+data = {
+    # Religion:
+    'Papa Francisco': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    },
+    'Create Religion': {
+        'status': '',
+        'price': '',
+        'affects': {
+           'happy': '',
+           'indifferent': '',
+           'unhappy': '',     
+            'angry': '',
+        },
+    },
+
+    # News:
+
+    # Education:
+    'UNB': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    },
+    'MIT': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    }
+
+    # Entertainement:
+    'Kino International': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    },
+    'Tate Modern': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    },
+    'World Cup': {
+        'status': '',
+        'price': '',
+        'affects': '',
+    }
+}
